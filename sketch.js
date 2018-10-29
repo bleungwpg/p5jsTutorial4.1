a843427cc0f9125844510c8c12d070bc082d0d35
Ben Leung <benjamin.leung80@gmail.com>
2018-10-29 13:49:49 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,25 +2,24 @@ var bgm;
 
 function preload()
 {
-	bgm = loadSound("slkdjf");
+	bgm = loadSound("https://bleungwpg.github.io/p5jsTutorial4.1/sounds/bensound-creativeminds.mp3");
 
 }
 
 function setup()
 {
 	createCanvas(800,600);
-
+	bgm.play();
 }
 
 function draw()
 {
-	textSize(16);
+	textSize(12);
 
 	background(125,125,125)
 
-	text("Credit for music goes to Benjamin TISSOT from www.bensound.com");
+	text("Credit for music goes to Benjamin TISSOT from www.bensound.com",10,40);
 
-	bgm.play();
 
 }
 
